a5638e860a3f07aa84b471ddcab3f95e726c8414
Anya <anyav09@gmail.com>
2011-01-02 20:43:55 -0800

    added world variables

--- rose.js
+++ rose.js
@@ -1,16 +1,46 @@
 ctx = null;
 
+//world variables
+
+hero = {x:10, y:10};
+villain = {x:700, y:10, dead:false};
+
+
 function main() {
-	console.log("Calling main");
 	var canvas = $("#main")[0];
 	ctx = canvas.getContext('2d');
 	
-	startJourney(10,10);
+	$('body').keydown(function(e){
+		console.log( e.keyCode );
+		if(e.keyCode == 37) {
+		//left arrow
+			hero.x = hero.x - 5;
+		}
+		if(e.keyCode == 39) {
+		//right arrow
+			hero.x = hero.x + 5;
+		}
+	});
+	
+	setInterval(function(){
+		updateWorld();
+		drawWorld();
+	},16);
 }
 
-function startJourney(x,y) {
-	drawHero(x,y);
-	setTimeout( function(){ startJourney(x+10, y+10); }, 1000);
+function updateWorld(){
+	
+	if( villain.x - hero.x <= 50 ) {
+		villain.dead = true;
+	}
+}
+
+function drawWorld(){
+	clearCanvas();
+	drawHero(hero.x,hero.y);
+	if(!villain.dead) {
+		drawVillain(villain.x,villain.y);
+	}
 }
 
 function drawRect(r,g,b,x,y,w,h) {
@@ -20,4 +50,12 @@ function drawRect(r,g,b,x,y,w,h) {
 
 function drawHero(x,y) {
 	drawRect(200, 0, 0, x, y, 50, 50);
+}
+
+function drawVillain(x,y) {
+	drawRect(0, 0, 0, x, y, 50, 50);
+}
+
+function clearCanvas() {
+	ctx.clearRect(0,0,$("#main")[0].width,$("#main")[0].height);
 }
